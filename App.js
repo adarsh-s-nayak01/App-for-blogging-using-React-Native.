3bc51e2bcc782ba97e3e58944fedcd97ab53e49a
adarsh-s-nayak <adarshsnayak2002@gmail.com>
2022-08-15 11:19:41 +0530

    done with all the screens

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
-import React from 'react'
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import IndexScreen from './src/screens/IndexScreen'
-import { BlogProvider } from "./src/context/BlogContext";
-
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import IndexScreen from './src/screens/IndexScreen';
+import ShowScreen from './src/screens/ShowScreen';
+import CreateScreen from './src/screens/CreateScreen';
+import EditScreen from './src/screens/EditScreen';
+import { Provider } from './src/context/BlogContext';
 
 const navigator = createStackNavigator(
-    {
-      Index: IndexScreen,
+  {
+    Index: IndexScreen,
+    Show: ShowScreen,
+    Create: CreateScreen,
+    Edit:EditScreen,
+  },
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs',
     },
-    {
-      initialRouteName: 'Index',
-      defaultNavigationOptions: {
-        title: 'Blogs',
-      },
-    }
-  );
-  
+  }
+);
 
 //this line only shows  initial routename
 const App = createAppContainer(navigator);
@@ -27,9 +31,9 @@ const App = createAppContainer(navigator);
 
 export default () => {
     return (
-      <BlogProvider>
+      <Provider>
         <App />
-      </BlogProvider>
+      </Provider>
     );
 };
 //  {/* //children in blogprovider is app, i.e navigator. */}
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,32 +1,59 @@
-import React,{useState} from 'react';
+import createDataContext from './createDataContext';
 
-const BlogContext = React.createContext();
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'edit_blogpost':
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
+    case 'delete_blogpost':
+      //if this is true, we'll include those blogposts in a new array.
+      return state.filter((blogPost) => blogPost.id !== action.payload);
+      //blogpost with payload id we'll be excluded.
+      //filter function will iterate over all the elements in the state array. then define a child function about what to do.
+    case 'add_blogpost':
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: action.payload.title,
+          content: action.payload.content,
+        },
+      ];
+    default:
+      return state;
+  }
+};
 
-//not using export default.
-//because we are exporting default -the BlogContext.
-export const BlogProvider = ({ children }) => {
-
-    // const blogPosts =[
-    //     {title:'Blog Post #1'},
-    //     {title:'Blog Post #2'},
-    //     {title:'Blog Post #3'},
-    //     // {title:'Blog Post #2'},
-    //     // {title:'Blog Post #2'},
-    // ]; this is static , 
-
-    const [blogPosts,setBlogPosts]=useState([]);
-
-    const addBlogPost =()=>{
-            setBlogPosts([
-                ...blogPosts,
-                { title:`Blog Post #${blogPosts.length+1}`}
-            ])//... means take all the blogposts from the array and make a new array.
+const addBlogPost = (dispatch) => {
+  return (title, content, callback) => {
+    dispatch({ type: 'add_blogpost', payload: { title, content } });
+     //type will taken by the react -native and then provided to blog reducer
+    //as second arguement.
+    if (callback) {
+      callback();
     }
-
-
-    return (
-      <BlogContext.Provider value={{data:blogPosts,addBlogPosts:addBlogPost}}>{children}</BlogContext.Provider>
-    );
   };
-  
-  export default BlogContext;
+};
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: 'delete_blogpost', payload: id });
+  };
+};
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({
+      type: 'edit_blogpost',
+      payload: { id, title, content },
+    });
+    if (callback) {
+      callback();
+    }
+  };
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
+);
